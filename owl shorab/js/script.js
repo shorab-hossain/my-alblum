db2da435c7ebf199fdf5f0401f1c6f7b40c7836e
shorab-hossain <72809340+shorab-hossain@users.noreply.github.com>
2020-10-17 11:51:36 +0600

    Add files via upload

---
+++ owl shorab/js/script.js
@@ -0,0 +1,25 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    autoplay:true,
+    autoplayTimeout:1000,
+    autoplayHoverPause:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        800:{
+            items:3
+        },
+        900:{
+            items:4
+        },
+        1000:{
+            items:5
+        }
+    }
+})
